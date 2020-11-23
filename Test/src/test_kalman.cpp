71c592c15da1dcc71e6d15899132de2171750976
Martin Moya <moyamartin1@gmail.com>
2020-11-23 17:42:40 -0300

    Adds unit test for kalman_filter

--- Test/src/test_kalman.cpp
+++ Test/src/test_kalman.cpp
@@ -4,59 +4,47 @@ extern "C" {
 #include "kalman_measurements.h"
 }
 
-static struct KalmanFilter soc_kalman_filter {
-	.x_act_data = 			{0.0, 70.71067812, 500.0, 70.71067812},
-	.f_mat_data = 			{1.0, 0.1, 0.0, 0.0,
-							  0.0, 1.0, 0.0, 0.0,
-							  0.0, 0.0, 1.0, 0.1,
-							  0.0, 0.0, 0.0, 1.0},
-	.g_mat_data = 			{0.0, 0.0, 0.0, 0.0,
-							 0.0, 0.0, 0.0, 0.0,
-							 0.0, 0.0, 1.0, 0.0,
-							 0.0, 0.0, 0.0, 1.0},
-	.p_mat_actual_data = 	{1.0, 0.0, 0.0, 0.0,
-							 0.0, 1.0, 0.0, 0.0,
-							 0.0, 0.0, 1.0, 0.0,
-							 0.0, 0.0, 0.0, 1.0},
-	.q_mat_data =			{0.0, 0.0, 0.0, 0.0,		
-							 0.0, 0.0, 0.0, 0.0,
-							 0.0, 0.0, 0.0, 0.0,
-							 0.0, 0.0, 0.0, 0.0},
-	.r_mat_data =			{0.2, 0.0, 0.0, 0.0,
-							 0.0, 0.2, 0.0, 0.0,
-							 0.0, 0.0, 0.2, 0.0,
-							 0.0, 0.0, 0.0, 0.2},
-	.h_mat_data =			{1.0, 0.0, 0.0, 0.0,
-							  0.0, 1.0, 0.0, 0.0,
-							  0.0, 0.0, 1.0, 0.0,
-							  0.0, 0.0, 0.0, 1.0},
-	.u_data =				{0.0, 0.0, -.0495, -.981}
-};
 
 TEST_GROUP(kalman_test)
 {
-	void setUp(){
-		uint8_t kalman_filter_is_init = kalman_filter_init(&soc_kalman_filter,
-			   											   4, 4, 4);
-		CHECK_EQUAL(1, kalman_filter_is_init);
+	struct KalmanFilter soc_kalman_filter;
+	void setup(){
+		// Actually this should be initialized as in /Core/Src/main.c
+		// but cpp-bullshit complains about initialization of structs due to
+		// design constraints
+		memcpy(soc_kalman_filter.x_act_data, &x_act_data_dummy, 
+			   N_STATES*sizeof(float32_t));
+		memcpy(soc_kalman_filter.f_mat_data, &f_mat_data_dummy, 
+			   N_STATES*N_STATES*sizeof(float32_t));
+		memcpy(soc_kalman_filter.g_mat_data, &g_mat_data_dummy, 
+			   N_STATES*N_INPUTS*sizeof(float32_t));
+		memcpy(soc_kalman_filter.h_mat_data, &h_mat_data_dummy,
+			   N_INPUTS*N_STATES*sizeof(float32_t));
+		memcpy(soc_kalman_filter.q_mat_data, &q_mat_data_dummy,
+			   N_STATES*N_STATES*sizeof(float32_t));
+		memcpy(soc_kalman_filter.p_mat_actual_data, &p_mat_actual_data_dummy,
+			   N_STATES*N_STATES*sizeof(float32_t));
+		memcpy(soc_kalman_filter.r_mat_data, &r_mat_data_dummy, 
+			   N_INPUTS*N_INPUTS*sizeof(float32_t));
+		memcpy(soc_kalman_filter.u_data, &u_data_dummy, 
+			   N_INPUTS*sizeof(float32_t));
+		kalman_filter_init(&soc_kalman_filter, 4, 4, 4);
 	}
 
-	void tearDown(){
-
+	void teardown(){
 	}
 };
 
 
-TEST (kalman_test, pass_me)
+TEST(kalman_test, kalman_pass_me)
 {	
-	kalman_filter_status kf_status;
-	for(int i = 0; i < NUM_SAMPLES; ++i){
-		kf_status = kalman_filter_step(&soc_kalman_filter, 
-									   measurements[i]);
-		CHECK_EQUAL(filtered_data[i][0], 
-					soc_kalman_filter.x_act.pData[0]);
-		CHECK_EQUAL(filtered_data[i][1],
-					soc_kalman_filter.x_act.pData[2]);
+	float32_t measurement_buffer[4];
+	for(int i = 0; i < N_SAMPLES; i++){
+		DOUBLES_EQUAL(filtered_data[i][0], 
+					  soc_kalman_filter.x_act.pData[0], 0.01);
+		DOUBLES_EQUAL(filtered_data[i][1],
+					  soc_kalman_filter.x_act.pData[2], 0.01);
+		memcpy(measurement_buffer, measurements[i], 4*sizeof(float32_t));
+		kalman_filter_step(&soc_kalman_filter, measurement_buffer);
 	}
 }
-
