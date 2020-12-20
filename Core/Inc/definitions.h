12ebf68b59a824c63266e08918a5d6bc38d51266
Martin Moya <moyamartin1@gmail.com>
2020-12-20 16:53:53 -0300

    ina226_drivers adds buffer_16b data structure as global definition

--- Core/Inc/definitions.h
+++ Core/Inc/definitions.h
@@ -1,12 +1,19 @@
-/*
- * definitions.h
+/******************************************************************************
  *
- *  Created on: 9 jul. 2020
- *      Author: Fededc88
- */
+ * @file	ina226_defs.h
+ * @brief 	Texas Instruments INA226 register and constants definitions
+ * @version	v1.00f00
+ * @date	16, Dec. 2020
+ * @author	CECARELLI, Federico (fededc88@gmail.com),
+ * 			MOYA, Martin		(moyamartin1@gmail.com),
+ * 			SANTOS, Lucio		(lusho2206@gmail.com)
+ * @copyright GPL license, all text here must be included in any redistribution
+ *****************************************************************************/
 
-#ifndef INC_DEFINITIONS_H_
-#define INC_DEFINITIONS_H_
+#ifndef	_DEFINITIONS_H_ 
+#define _DEFINITIONS_H_ 
+
+#include <stdint.h>
 
 #ifndef TRUE
 #define TRUE 1
@@ -15,5 +22,19 @@
 #ifndef FALSE
 #define FALSE 0
 #endif /* FALSE */
+
+
+/**
+ * @union 16b_buffer;
+ * @brief Union representing a buffer of 16 bits, that allows you to access to
+ * 		  the low and high 8 bits of a uint16_t 
+ */
+typedef union { 
+	uint16_t b_all;
+	struct {
+		uint8_t b_low;
+		uint8_t b_high;
+	};
+} buffer_16b;
 
 #endif /* INC_DEFINITIONS_H_ */
