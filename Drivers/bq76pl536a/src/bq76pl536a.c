b9c9968b982234b1b667cd39d5a4a442708a569a
Martin Moya <moyamartin1@gmail.com>
2021-01-01 19:06:03 -0300

    Adds static functions for SPI read and write

--- Drivers/bq76pl536a/src/bq76pl536a.c
+++ Drivers/bq76pl536a/src/bq76pl536a.c
@@ -1,7 +1,7 @@
 #include "bq76pl536a.h"
 
 /**
- * @func BQ76_write_packet_format
+ * @func init_write_packet
  * @brief It generates a BQ76_write_packet_format struct given the device 
  * 	  	  address, register address and register data
  * @params[in] device_address: uint8_t bq76 device address
@@ -9,8 +9,9 @@
  * @params[in] reg_data: uint8_t packet data to write
  * @return BQ76_write_packet_format
  */
-static struct BQ76_write_packet_format init_write_packet(device_address, 
-														 reg_address, reg_data)
+static struct BQ76_write_packet_format init_write_packet(uint8_t device_address, 
+														 uint8_t reg_address, 
+														 uint8_t reg_data)
 {
 	struct BQ76_write_packet_format packet;
 	packet.device_address = device_address;
@@ -20,30 +21,79 @@ static struct BQ76_write_packet_format init_write_packet(device_address,
 }
 
 /**
- * @func BQ76_writereg
+ * @func BQ76_read_packet_format
+ * @brief It generates a BQ76_read_packet_format struct given the device 
+ * 	  	  address, start register address and the length of the packet 
+ * @params[in] device_address: uint8_t bq76 device address
+ * @params[in] start_reg_address: uint8_t start register address to read
+ * @params[in] read_length: uint8_t length of data to read
+ * @return BQ76_read_packet_format
+ */
+static struct BQ76_read_packet_format init_read_packet(uint8_t device_address, 
+														uint8_t start_reg_addr, 
+														uint8_t read_length)
+{
+	struct BQ76_read_packet_format packet;
+	packet.device_address = device_address;
+	packet.start_reg_address = start_reg_addr;
+	packet.read_length = read_length;
+	return packet;
+}
+
+
+/**
+ * @func writereg
  * @brief Write a register of the BQ76PL536 device.
  * 		  The user of this library is responsible of defining this function. As
  * 		  an example, we have only defined functionality for STM32F407xx
  * @params[in] spi_address: BQ76 device address
  * @params[in] reg: register address to write
  * @params[in] value: value to write
- * @return BQ76_status
+ * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
  */
-static enum BQ76_status BQ76_writereg(uint8_t spi_address, uint8_t reg_address, 
+static enum BQ76_status writereg(uint8_t spi_address, uint8_t reg_address, 
 									  uint8_t reg_data)
 {
-	struct BQ76_write_packet_format = init_write_packet(spi_address, 
-														reg_address,
-														reg_data);	
+	struct BQ76_write_packet_format packet = init_write_packet(spi_address, 
+															   reg_address,
+															   reg_data);	
 #if defined(USE_HAL_DRIVER) && defined(STM32F407xx)
-	HAL_SPI_Transmit(&BQ76_INTERFACE, )
+	if(HAL_SPI_Transmit(&BQ76_INTERFACE, (uint8_t *) &packet, 
+					 BQ76_TX_BUFF_SIZE, BQ76_TIMEOUT) != HAL_OK)
+	{
+		return SPI_TRANSMISSION_ERROR;
+	}
+	return OK;
 #endif
 }
 
-
-
-static enum BQ76_status BQ76_readreg(uint8_t spi_address, uint8_t reg_address,
-									 uint8_t * value)
+/**
+ * @func readreg
+ * @brief Write a register of the BQ76PL536 device.
+ * 		  The user of this library is responsible of defining this function. As
+ * 		  an example, we have only defined functionality for STM32F407xx.
+ * 		  Make sure that data has the size of read_length, otherwise it would be
+ * 		  unsafe because the SPI receive function will overwrite over the
+ * 		  overflowed address
+ * @params[in] spi_address: BQ76 device address
+ * @params[in] reg: register address to write
+ * @params[in] value: value to write
+ * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
+ */
+static enum BQ76_status readreg(uint8_t spi_address, uint8_t reg_address,
+								uint8_t read_length, uint8_t * data)
 {
-
+#if defined(USE_HAL_DRIVER) && defined(STM32F407xx)
+	struct BQ76_read_packet_format packet = init_read_packet(spi_address,
+															 reg_address,
+															 read_length);
+	if(HAL_SPI_Transmit(&BQ76_INTERFACE, (uint8_t *) &packet, BQ76_TX_BUFF_SIZE,
+						BQ76_TIMEOUT) != HAL_OK){
+		return SPI_TRANSMISSION_ERROR;
+	}
+	if(HAL_SPI_Receive(&BQ76_INTERFACE, rx_packet, read_length, 
+				BQ76_TIMEOUT) != HAL_OK){
+		return SPI_TRANSMISSION_ERROR:
+	}
+	return OK;
 }
