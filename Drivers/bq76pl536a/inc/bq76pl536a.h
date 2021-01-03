f49b411993b7dbfb39ef086b929d2af2352d5f7f
Martin Moya <moyamartin1@gmail.com>
2021-01-03 20:19:30 -0300

    Adds OT/OTT functions

--- Drivers/bq76pl536a/inc/bq76pl536a.h
+++ Drivers/bq76pl536a/inc/bq76pl536a.h
@@ -50,6 +50,9 @@ enum BQ76_Status {
 	COV_CONFIG_FAIL = -9,
 	COVT_CONFIG_FAIL = -10,
 	CUV_CONFIG_FAIL = -11,
+	CUVT_CONFIG_FAIL = -12,
+	OT_CONFIG_FAIL = -13,
+	OTT_CONFIG_FAIL = -14,
 };
 
 struct BQ76_write_packet_format {
@@ -65,28 +68,28 @@ struct BQ76_read_packet_format {
 }
 
 struct BQ76 {
-	struct BQ76 * north;
-	struct BQ76 * south;
-	struct adc_control;
-	struct adc_convert;
+	struct adc_control adc_control;
+	struct adc_convert adc_convert;
 	struct address_control address_control;
-	struct alert_status;
-	struct cb_ctrl;
-	struct cb_time;
-	struct cov_config;
-	struct cov_fault;
-	struct covt_config;
-	struct cuv_config;
-	struct cuv_fault;
-	struct cuvt_config;
+	struct alert_status alert_status;
+	struct cb_ctrl cb_ctrl;
+	struct cb_time cb_time;
+	struct cov_fault cov_fault;
+	struct cuv_fault cuv_fault;
+	struct delay_config covt_config;
+	struct delay_config cuvt_config;
 	struct device_status device_status;
-	struct function_config;
-	struct io_config;
-	struct io_control;
-	struct presult_a;
-	struct presult_b;
-	uint8_t * host;
-	uint8_t shadow_control; /**< to enable group3 regs write 0x35 to it*/
+	struct function_config function_config;
+	struct io_config io_config;
+	struct io_control io_control;
+	struct ot_config ot_config;
+	struct presult_a presult_a;
+	struct presult_b presult_b;
+	struct vth_config cov_config;
+	struct vth_config cuv_config;
+	uint8_t ott_config ott_config;
+	uint8_t shadow_control shadow_control; /**< to enable group3 regs write 0x35 to it*/
+	uint8_t user_register user_register[4];
 };
 
 /**
@@ -96,7 +99,28 @@ struct BQ76 {
  * 					   initialized 
  * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
  */
-enum BQ76_status bq76_init(struct BQ76 * device);
+enum BQ76_status bq76_init(struct BQ76 * device, uint8_t new_spi_address,
+						   enum bat_series_inputs num_bat_series,
+						   enum temp_sensor_inputs ts, uint8_t gpai,
+						   uint8_t balancing_time_unit, uint8_t balancing_time,
+						   uint8_t gpai_ref, uint8_t gpai_src,
+						   enum series_cells series_cells,
+						   uint8_t crc_enable, uint8_t crc_assert_pin,
+						   uint8_t cov_disable, uint8_t cov_threshold,
+						   uint8_t covt_time_unit, uint8_t covt_delay,
+						   uint8_t cuv_disable, uint8_t cuv_threshold,
+						   uint8_t temp1_threshold, uint8_t temp2_threshold,
+						   uint16_t temp_delay);
+
+/**
+ * @func bq76_init
+ * @brief Initializes a bq76 struct with default settings
+ * @params[in] device: BQ76 pointer referencing to the desired device to be
+ * 					   initialized 
+ * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
+ */
+enum BQ76_status bq76_init_default(struct BQ76 * device, 
+								   uint8_t new_spi_address)
 
 /**
  * @func bq76_broadcast_reset
@@ -126,7 +150,7 @@ enum BQ76_status bq76_reset(struct BQ76 * device);
 enum BQ76_status bq76_set_address(struct BQ76 * device);
 
 /**
- * @func bq76_set_cb_time
+ * @func  bq76_set_cb_time
  * @brief sets a device's balancing timeout
  * @params[in] device: BQ76 pointer referencing to the desired device to be
  * 					   modified
@@ -141,7 +165,7 @@ enum BQ76_status bq76_set_cb_time(struct BQ76 * device,
 
 
 /**
- * @func bq76_set_function_config
+ * @func  bq76_set_function_config
  * @brief sets a device's function config register
  * @params[in] device: BQ76 pointer referencing to the desired device to be
  * 					   modified
@@ -158,7 +182,7 @@ enum BQ76_status bq76_set_function_config(struct BQ76 * device,
 										  enum series_cells series_cells);
 
 /**
- * @func bq76_set_io_config
+ * @func  bq76_set_io_config
  * @brief sets a device's I/O config register
  * @params[in] device: BQ76 pointer referencing to the desired device to be
  * 					   modified
@@ -173,7 +197,7 @@ enum BQ76_status bq76_set_io_config(struct BQ76 * device,
 									uint8_t crc_enable, uint8_t crc_assert_pin);
 
 /**
- * @func bq76_set_cov_config
+ * @func  bq76_set_cov_config
  * @brief sets a device's Cell Overvoltage Threshold value
  * @params[in] device: BQ76 pointer referencing to the desired device to be
  * 			   resetted
@@ -186,22 +210,7 @@ enum BQ76_status bq76_set_cov_config(struct BQ76 * device, uint8_t disable,
 									 float32_t voltage_threshold);
 
 /**
- * @func bq76_set_covt_config
- * @brief sets a device's Cell Overvoltage delay time
- * @params[in] device: BQ76 pointer referencing to the desired device to be
- * 			   modified
- * @params[in] time_unit: uint8_t variable that determines the units of the
- * 			   delay time, microseconds or miliseconds 
- * 			   (0: microseconds, 1: miliseconds)
- * @params[in] voltage_threshold: uint16_t variable that holds the 
- * 								  delay time
- * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
- */
-enum BQ76_status bq76_set_covt_config(struct BQ76 * device, uint8_t time_unit, 
-									  uint16_t delay); 
-
-/**
- * @func bq76_set_cuv_config
+ * @func  bq76_set_cuv_config
  * @brief sets a device's Cell undervoltage threshold value
  * @params[in] device: BQ76 pointer referencing to the desired device to be
  * 					   modified
@@ -209,4 +218,55 @@ enum BQ76_status bq76_set_covt_config(struct BQ76 * device, uint8_t time_unit,
 enum BQ76_status bq76_set_cuv_config(struct BQ76 * device, uint8_t disable,
 									 float32_t voltage_threshold);
 
-#endif 
+/**
+ * @func  bq76_set_covt_config
+ * @brief sets a device's Cell Undervoltage delay time
+ * @params[in] device: BQ76 pointer referencing to the desired device to be
+ * 			   modified
+ * @params[in] time_unit: uint8_t variable that determines the units of the
+ * 			   delay time, microseconds or miliseconds 
+ * 			   (0: microseconds, 1: miliseconds)
+ * @params[in] delay: uint16_t variable that holds the 
+ * 					  delay time
+ * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
+ */
+enum BQ76_status bq76_set_covt_config(struct BQ76 * device, uint8_t time_unit, 
+									  uint16_t delay); 
+
+/**
+ * @func  bq76_set_cuvt_config
+ * @brief sets a device's Cell Undervoltage delay time
+ * @params[in] device: BQ76 pointer referencing to the desired device to be
+ * 			   modified
+ * @params[in] time_unit: uint8_t variable that determines the units of the
+ * 			   delay time, microseconds or miliseconds 
+ * 			   (0: microseconds, 1: miliseconds)
+ * @params[in] delay: uint16_t variable that holds the 
+ * 					  delay time
+ * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
+ */
+enum BQ76_status bq76_set_cuvt_config(struct BQ76 * device, uint8_t time_unit, 
+									  uint16_t delay); 
+
+/**
+ * @func  bq76_set_ot_config
+ * @brief sets a device's temperature sensor threshold value for sensors 1 and 2
+ * @params[in] device: BQ76 pointer referencing to the desired device to be
+ * 			   modified
+ * @params[in] ot1: uint8_t value that sets the temp sensor 1 threshold value
+ * @params[in] ot2:
+ */
+enum BQ76_status bq76_set_ot_config(struct BQ76 * device, uint8_t ot1, 
+									uint8_t ot2);
+
+/**
+ * @func  bq76_set_ott_config
+ * @brief sets a device's temperature sensor delay time detection for temp.
+ * 		  sensors 1 and 2
+ * @params[in] device: BQ776 pointer referencing to the desired device to be
+ * 					   modified
+ * @params[in] 
+ */
+enum BQ76_status bq76_set_ott_config(struct BQ76 * device, uint8_t delay_time);
+
+#endif
