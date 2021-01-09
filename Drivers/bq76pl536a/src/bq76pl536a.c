dbd472cea4f664c8c7dffbab9175929be3c3ac2c
Martin Moya <moyamartin1@gmail.com>
2021-01-09 18:18:54 -0300

    mmoya/bq76_drivers Fixes compilation errors

--- Drivers/bq76pl536a/src/bq76pl536a.c
+++ Drivers/bq76pl536a/src/bq76pl536a.c
@@ -10,7 +10,7 @@
  */
 static uint8_t calculate_ott(uint16_t delay_time)
 {
-	delay_time = (delay_time > 2550) ? 2550 : delay_time;
+	delay_time = (delay_time > MAX_OTT_DELAY) ? MAX_OTT_DELAY : delay_time;
 	return delay_time/OTT_LSB_VALUE;
 }
 
@@ -32,7 +32,7 @@ static uint8_t calculate_ot(uint8_t temperature_threshold)
 	if(temperature_threshold < MIN_CAL_TEMP){
 		temperature_threshold = MIN_CAL_TEMP;
 	}
-	return 1 + ((temperature_threshold - MIN_CAL_TEMP)/CAL_TEMP_LSB);
+	return (1 + ((temperature_threshold - MIN_CAL_TEMP)/CAL_TEMP_LSB));
 }
 
 /**
@@ -119,8 +119,8 @@ static struct BQ76_write_packet_format init_write_packet(uint8_t device_address,
  * @return BQ76_read_packet_format
  */
 static struct BQ76_read_packet_format init_read_packet(uint8_t device_address, 
-														uint8_t start_reg_addr, 
-														uint8_t read_length)
+													   uint8_t start_reg_addr, 
+													   uint8_t read_length)
 {
 	struct BQ76_read_packet_format packet;
 	packet.device_address = device_address;
@@ -137,7 +137,7 @@ static struct BQ76_read_packet_format init_read_packet(uint8_t device_address,
  * @params[in] spi_address: BQ76 device address
  * @params[in] reg: register address to write
  * @params[in] value: value to write
- * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
+ * @return BQ76_status [BQ76_OK|BQ76_SPI_TRANSMISSION_ERROR]
  */
 static enum BQ76_status writereg(uint8_t spi_address, uint8_t reg_address, 
 									  uint8_t reg_data)
@@ -149,9 +149,9 @@ static enum BQ76_status writereg(uint8_t spi_address, uint8_t reg_address,
 	if(HAL_SPI_Transmit(&BQ76_INTERFACE, (uint8_t *) &packet, 
 					 BQ76_TX_BUFF_SIZE, BQ76_TIMEOUT) != HAL_OK)
 	{
-		return SPI_TRANSMISSION_ERROR;
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	return OK;
+	return BQ76_OK;
 #endif
 }
 
@@ -166,7 +166,7 @@ static enum BQ76_status writereg(uint8_t spi_address, uint8_t reg_address,
  * @params[in] spi_address: BQ76 device address
  * @params[in] reg: register address to write
  * @params[in] value: value to write
- * @return BQ76_status [OK|SPI_TRANSMISSION_ERROR]
+ * @return BQ76_status [BQ76_OK|BQ76_SPI_TRANSMISSION_ERROR]
  */
 static enum BQ76_status readreg(uint8_t spi_address, uint8_t reg_address,
 								uint8_t read_length, uint8_t * data)
@@ -177,273 +177,245 @@ static enum BQ76_status readreg(uint8_t spi_address, uint8_t reg_address,
 															 read_length);
 	if(HAL_SPI_Transmit(&BQ76_INTERFACE, (uint8_t *) &packet, BQ76_TX_BUFF_SIZE,
 						BQ76_TIMEOUT) != HAL_OK){
-		return SPI_TRANSMISSION_ERROR;
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
 	if(HAL_SPI_Receive(&BQ76_INTERFACE, data, read_length, 
 					   BQ76_TIMEOUT) != HAL_OK){
-		return SPI_TRANSMISSION_ERROR:
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	return OK;
+	return BQ76_OK;
 #endif
 }
 
-enum BQ76_status bq76_init(struct BQ76 * device, uint8_t new_spi_address,
-						   enum bat_series_inputs num_bat_series,
-						   enum temp_sensor_inputs ts, uint8_t gpai,
-						   uint8_t balancing_time_unit, uint8_t balancing_time,
-						   uint8_t gpai_ref, uint8_t gpai_src,
-						   enum series_cells series_cells,
-						   uint8_t crc_enable, uint8_t crc_assert_pin,
-						   uint8_t cov_disable, uint8_t cov_threshold,
-						   uint8_t covt_time_unit, uint8_t covt_delay,
-						   uint8_t cuv_disable, uint8_t cuv_threshold,
-						   uint8_t temp1_threshold, uint8_t temp2_threshold,
-						   uint16_t ott_temp_delay)
+enum BQ76_status bq76_init(struct BQ76 * device, uint8_t spi_address,
+						   uint8_t balancing_time, float32_t cov_threshold, 
+						   uint8_t covt_delay, float32_t cuv_threshold, 
+						   uint8_t cuvt_delay, float32_t temp1_threshold, 
+						   float32_t temp2_threshold, uint16_t temp_delay)
 {
 	// Send broadcast reset
-	if(broadcast_reset() != OK){
-		return BROADCAST_RESET_FAIL;
+	if(bq76_broadcast_reset() != BQ76_OK){
+		return BQ76_BROADCAST_RESET_FAIL;
 	}
 
 	// set address to device
-	if(bq76_set_address(device, new_spi_address) != OK){
-		return ADDRESS_CONFIG_FAIL;
+	if(bq76_set_address(device, spi_address) != BQ76_OK){
+		return BQ76_ADDRESS_CONFIG_FAIL;
 	}
 
 	// config adc control register
-	if(bq76_set_adc_control(device, num_bat_series, ts, gpai) != OK){
-		return ADC_CONFIG_FAIL;
+	if(bq76_set_adc_control(device) != BQ76_OK){
+		return BQ76_ADC_CONFIG_FAIL;
 	}
 	
 	// config balancing time outputs
-	if(bq76_set_cb_time(device, balancing_time_unit, balancing_time) != OK){
-		return CB_TIME_CONFIG_FAIL;
+	if(bq76_set_cb_time(device, balancing_time) != BQ76_OK){
+		return BQ76_CB_TIME_CONFIG_FAIL;
 	}
 
 	// set function configuration
-	if(bq76_set_cb_time(device, gpai_ref, gpai_src, series_cells) != OK){
-		return FUNCTION_CONFIG_FAIL;
+	if(bq76_set_function_config(device) != BQ76_OK){
+		return BQ76_FUNCTION_CONFIG_FAIL;
 	}
 
 	// set I/O configuration
-	if(bq76_set_io_config(device, crc_enable, crc_assert_pin) != OK){
-		return IO_CONFIG_FAIL;
+	if(bq76_set_io_config(device) != BQ76_OK){
+		return BQ76_IO_CONFIG_FAIL;
 	}
 
 	// set cov config
-	if(bq76_set_cov_config(device, cov_disable, voltage_threshold) != OK){
-		return COV_CONFIG_FAIL;
+	if(bq76_set_cov_config(device, cov_threshold) != BQ76_OK){
+		return BQ76_COV_CONFIG_FAIL;
 	}
 
 	// set covt config
-	if(bq76_set_covt_config(device, covt_time_unit, covt_delay) != OK){
-		return COVT_CONFIG_FAIL;
+	if(bq76_set_covt_config(device, covt_delay) != BQ76_OK){
+		return BQ76_COVT_CONFIG_FAIL;
 	}
 
 	// set cuv config
-	if(bq76_set_cuv_config(device, cuv_disable, cuv_threshold) != OK){
-		return CUV_CONFIG_FAIL;
+	if(bq76_set_cuv_config(device, cuv_threshold) != BQ76_OK){
+		return BQ76_CUV_CONFIG_FAIL;
 	}
 
 	// set cuvt config
-	if(bq76_set_cuvt_config(device, cuvt_time_unit, cuvt_delay) != OK){
-		return CUVT_CONFIG_FAIL;
+	if(bq76_set_cuvt_config(device, cuvt_delay) != BQ76_OK){
+		return BQ76_CUVT_CONFIG_FAIL;
 	}
 
 	// set ot config
-	if(bq76_set_ot_config(device, temp1_threshold, temp2_threshold) != OK){
-		return OT_CONFIG_FAIL;
+	if(bq76_set_ot_config(device, temp1_threshold, temp2_threshold) != BQ76_OK){
+		return BQ76_OT_CONFIG_FAIL;
 	}
 
 	// set ott config
-	if(bq76_set_ott_config(device, ott_temp_delay) != OK){
-		return OTT_CONFIG_FAIL;
+	if(bq76_set_ott_config(device, temp_delay) != BQ76_OK){
+		return BQ76_OTT_CONFIG_FAIL;
 	}
-	return OK;
-}
-
-enum BQ76_status bq76_init_default(){
-
+	return BQ76_OK;
 }
 
 enum BQ76_status bq76_broadcast_reset()
 {
-	if(writereg(BROADCAST_ADDRESS, RESET_REG, RESET_DEVICE_VALUE) != OK){
-		return SPI_TRANSMISSION_ERROR;
+	if(writereg(BROADCAST_ADDRESS, RESET_REG, RESET_DEVICE_VALUE) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	return OK;
+	return BQ76_OK;
 }
 
 enum BQ76_status bq76_reset(struct BQ76 * device)
 {
 	if(writereg((uint8_t) device->address_control.ADDR, RESET_REG, 
-				RESET_DEVICE_VALUE) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				RESET_DEVICE_VALUE) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	return OK;
+	return BQ76_OK;
 }
 
 enum BQ76_status bq76_set_address(struct BQ76 * device, uint8_t address)
 {
 	// Write new address to device
-	if(writreg(device->address, ADDRESS_CONTROL_REG, address) != OK){
-		return SPI_TRANSMISSION_ERROR;
+	if(writereg(device->address_control.ADDR, ADDRESS_CONTROL_REG, 
+				address) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
 	// Check if the address has been correctly set
 	struct address_control address_buffer;
 	// 1 - read new address
-	if(readreg(new_address, ADDRESS_CONTROL_REG, 1, &address_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+	if(readreg(address, ADDRESS_CONTROL_REG, 1, 
+			(uint8_t *) &address_buffer) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
 	// 2 - check if the ADDR_RQST is not set or the ADDR[n] is different from 
 	// the new address
 	if(!address_buffer.ADDR_RQST || 
-			(uint8_t) address_buffer->ADDR != new_address){
-		return ADDRESS_CONFIG_FAIL;
+			(uint8_t) address_buffer.ADDR != address){
+		return BQ76_ADDRESS_CONFIG_FAIL;
 	}
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_adc_control(struct BQ76 * device, 
-									  enum bat_series_inputs num_bat_series, 
-									  enum temp_sensor_inputs ts, uint8_t gpai)
+enum BQ76_status bq76_set_adc_control(struct BQ76 * device)
 {
-	struct adc_control adc_control_buffer;
-	adc_control_buffer.CELL_SEL = num_bat_series;
-	adc_control_buffer.GPAI = gpai;
-	adc_control_buffer.TS = ts;
+	uint8_t adc_control_data = (device->adc_control.CELL_SEL << 6)	| 
+				   			   (device->adc_control.GPAI << 5)		| 
+				   			   (device->adc_control.TS << 4)		| 
+				   			   (device->adc_control.GPAI << 3)		| 
+				   			   device->adc_control.CELL_SEL;
 	if(writereg((uint8_t) device->address_control.ADDR, ADC_CONTROL_REG, 
-				(uint8_t) adc) != OK){
-		return SPI_TRANSMISSSION_ERROR;
+				adc_control_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	// update the BQ76 device_status local reg
-	device->adc_control = adc_control_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_cb_time(struct BQ76 * device,
-								  uint8_t mins_secs, uint8_t balancing_time)
+enum BQ76_status bq76_set_cb_time(struct BQ76 * device, uint8_t balancing_time)
 {
-	struct cb_time;
-	cb_time.CBCT = mins_secs;
-	cb_time.CBT = (balancing_time > 63) ? 63 : balancing_time;
+	device->cb_time.CBT = (balancing_time > 63) ? 63 : balancing_time;
+	uint8_t cb_time_data = device->cb_time.CBCT << 7 | device->cb_time.CBT;
 	if(writereg((uint8_t) device->address_control.ADDR, CB_TIME_REG, 
-				(uint8_t) cb_time) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				cb_time_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->cb_time = cb_time;
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_function_config(struct BQ76 * device,
-										  uint8_t gpai_ref, uint8_t gpai_src,
-										  enum series_cells series_cells)
+enum BQ76_status bq76_set_function_config(struct BQ76 * device)
 {
-	struct function_config function_config_buffer;
-	function_config_buffer.CN = series_cells;
-	function_config_buffer.GPAI_SRC = gpai_src;
-	function_config_buffer.GPAI_REF = gpai_ref;
+	uint8_t function_config_data =	(device->function_config.GPAI_REF << 5) | 
+				 				    (device->function_config.GPAI_SRC << 4) |
+									(device->function_config.CN << 2);
 	if(writereg((uint8_t) device->address_control.ADDR, FUNCTION_CONFIG_REG,
-				(uint8_t) function_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				(uint8_t) function_config_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->function_config = function_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_io_config(struct BQ76 * device, uint8_t crc_enable,
-									uint8_t crc_assert_pin)
+enum BQ76_status bq76_set_io_config(struct BQ76 * device)
 {
-	struct io_config io_config_buffer;
-	io_config_buffer.CRC_DIS = crc_enable;
-	io_config_buffer.CRCNOFLT = crc_assert_pin;
+	uint8_t io_config_data = (device->io_config.CRCNOFLT << 7) | 
+				   			 (device->io_config.CRC_DIS);
 	if(writereg((uint8_t) device->address_control.ADDR, IO_CONFIG_REG,
-				(uint8_t) io_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				io_config_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->io_config = io_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_cov_config(struct BQ76 * device, uint8_t disable,
+enum BQ76_status bq76_set_cov_config(struct BQ76 * device, 
 									 float32_t voltage_threshold)
 {
-	struct vth_config cov_config_buffer;
-	cov_config_buffer.COV = calculate_cov(voltage_threshold);
-	cov_config_buffer.DISABLE = disable;
+	device->cov_config.VTH = calculate_cov(voltage_threshold);
+	uint8_t cov_config_data = (device->cov_config.DISABLE << 7) | 
+							  (device->cov_config.VTH);
 	if(writereg((uint8_t) device->address_control.ADDR, COV_CONFIG_REG, 
-				(uint8_t) cov_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				cov_config_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->cov_config = cov_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_cuv_config(struct BQ76 * device, uint8_t disable,
+enum BQ76_status bq76_set_cuv_config(struct BQ76 * device, 
 									 float32_t voltage_threshold)
 {
-	struct vth_config cuv_config_buffer;
-	cuv_config_buffer.CUV = calculate_cuv(voltage_threshold);
-	cuv_config_buffer.DISABLED = disable;
+	device->cov_config.VTH = calculate_cuv(voltage_threshold);
+	uint8_t cov_config_data = (device->cov_config.DISABLE << 7) | 
+							  device->cov_config.VTH;
 	if(writereg((uint8_t) device->address_control.ADDR, CUV_CONFIG_REG,
-			    (uint8_t) cuv_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+			    cov_config_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->cuv_config = cuv_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_covt_config(struct BQ76 * device,	uint8_t time_unit,
-									  float32_t delay)
+enum BQ76_status bq76_set_covt_config(struct BQ76 * device,	uint16_t delay)
 {
-	struct delay_config covt_config_buffer;
-	covt_config_buffer.US_MS = time_unit;
-	covt_config_buffer.CUVD = calculate_delay(delay);
+	device->covt_config.DELAY = calculate_delay(delay);
+	uint8_t covt_config_data = (device->covt_config.US_MS << 7) |
+							   device->covt_config.DELAY;
 	if(writereg((uint8_t) device->address_control.ADDR, COVT_CONFIG_REG, 
-				(uint8_t) cuvt_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				covt_config_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->cuvt_config = cuvt_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
-enum BQ76_status bq76_set_cuvt_config(struct BQ76 * device,	uint8_t time_unit,
-									  float32_t delay)
+enum BQ76_status bq76_set_cuvt_config(struct BQ76 * device, uint16_t delay)
 {
-	struct delay_config cuvt_config_buffer;
-	cuvt_config_buffer.US_MS = time_unit;
-	cuvt_config_buffer.CUVD = calculate_delay(delay);
+	device->cuvt_config.DELAY = calculate_delay(delay);
+	uint8_t cuvt_config_data = (device->cuvt_config.US_MS << 7) |
+							   (device->cuvt_config.DELAY);
 	if(writereg((uint8_t) device->address_control.ADDR, CUVT_CONFIG_REG, 
-				(uint8_t) cuvt_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				cuvt_config_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->cuvt_config = cuvt_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
 enum BQ76_status bq76_set_ot_config(struct BQ76 * device, 
 									float32_t ot1_threshold, 
 									float32_t ot2_threshold)
 { 
-	struct ot_config ot_config_buffer;
-	ot_config_buffer.OT1 = calculate_ot(ot1_threshold);
-	ot_config_buffer.OT2 = calculate_ot(ot2_threshold);
-	if(writreg((uint8_t) device->address_control.ADDR, OT_CONFIG_REG,
-			   (uint8_t) ot_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+	device->ot_config.OT1 = calculate_ot(ot1_threshold);
+	device->ot_config.OT2 = calculate_ot(ot2_threshold);
+	uint8_t ot_config_data = (device->ot_config.OT2 << 4) |
+							 (device->ot_config.OT1);
+	if(writereg((uint8_t) device->address_control.ADDR, OT_CONFIG_REG,
+			   ot_config_data) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->ot_config = ot_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
 
 enum BQ76_status bq76_set_ott_config(struct BQ76 * device,
 									 uint16_t delay_value)
 {
-	uint8_t ott_config_buffer = delay_value;
+	// the LSB of ott_config representes 10mS
+	device->ott_config = calculate_ott(delay_value);
 	if(writereg((uint8_t) device->address_control.ADDR, OTT_CONFIG_REG,
-				ott_config_buffer) != OK){
-		return SPI_TRANSMISSION_ERROR;
+				device->ott_config) != BQ76_OK){
+		return BQ76_SPI_TRANSMISSION_ERROR;
 	}
-	device->ott_config = ott_config_buffer;
-	return OK;
+	return BQ76_OK;
 }
--- Drivers/ina226/src/ina226.c
+++ Drivers/ina226/src/ina226.c
@@ -19,9 +19,9 @@ static enum INA226_status writereg(enum INA226_i2c_addresses i2c_address,
 	if(HAL_I2C_Mem_Write(&INA226_INTERFACE, (uint16_t) i2c_address,
 							   reg, INA226_ADDRESS_SIZE, (uint8_t *) &value,
 							   INA226_TX_BUFF_SIZE, INA226_TIMEOUT) != HAL_OK){
-		return I2C_TRANSMISSION_ERROR;
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
-	return OK;
+	return INA226_OK;
 
 #elif defined(__AVR__)
 #endif
@@ -42,10 +42,10 @@ static enum INA226_status readreg(enum INA226_i2c_addresses i2c_address,
 	if(HAL_I2C_Mem_Read(&INA226_INTERFACE, (uint16_t) i2c_address, 
 							  reg, INA226_ADDRESS_SIZE, (uint8_t *)&rx_data, 
 							  INA226_RX_BUFF_SIZE, INA226_TIMEOUT) != HAL_OK) {
-		return I2C_TRANSMISSION_ERROR;
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
 	*value = ((rx_data >> 8) & 0x00FF) | ((rx_data << 8) & 0xFF00);
-	return OK;
+	return INA226_OK;
 #elif defined(__AVR__)
 #endif
 }
@@ -103,19 +103,19 @@ enum INA226_status ina226_init(struct INA226 * ina226,
 	 * chip
 	 */
 	uint16_t man_id_val;
-	if(readreg(address, INA226_MAN_ID_REG, &man_id_val) != OK){
-		return I2C_TRANSMISSION_ERROR;
+	if(readreg(address, INA226_MAN_ID_REG, &man_id_val) != INA226_OK){
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
 
 	if(man_id_val != INA226_MAN_ID_VAL){
-		return MAN_ID_MISMATCH;
+		return INA226_MAN_ID_MISMATCH;
 	}
 	uint16_t die_id_val;
-	if(readreg(address, INA226_DIE_ID_REG, &die_id_val) != OK){
-		return I2C_TRANSMISSION_ERROR;
+	if(readreg(address, INA226_DIE_ID_REG, &die_id_val) != INA226_OK){
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
 	if(die_id_val != INA226_DIE_ID_VAL){
-		return DIE_ID_MISMATCH;
+		return INA226_DIE_ID_MISMATCH;
 	}
 
 	ina226->address = address;
@@ -129,9 +129,8 @@ enum INA226_status ina226_init(struct INA226 * ina226,
 	// configuration is different from the default values
 	if(config_buffer.buffer.all != INA226_CONFIG_DEFAULT){
 		if(writereg(ina226->address, INA226_CONFIG_REG, 
-						   config_buffer.buffer.all) != 
-				OK){
-			return CONFIG_ERROR;
+						   config_buffer.buffer.all) != INA226_OK){
+			return INA226_CONFIG_ERROR;
 		}
 	}
 	uint16_t buffer;
@@ -140,18 +139,18 @@ enum INA226_status ina226_init(struct INA226 * ina226,
 	ina226->current_active_mode = mode;
 
 	if(ina226_set_calibration(ina226, r_shunt, max_expected_current) 
-			!= OK){
-		return CAL_ERROR;
+			!= INA226_OK){
+		return INA226_CAL_ERROR;
 	}
 	readreg(ina226->address, INA226_CAL_REG, &buffer);
 
 	// set ina226 mask/enable register data
 	if(writereg(ina226->address, INA226_MASK_EN_REG, mask_enable) != 
-			OK){
-		return MASK_EN_ERROR;
+			INA226_OK){
+		return INA226_MASK_EN_ERROR;
 	}
 	ina226->mask_enable = mask_enable;
-	return OK;	
+	return INA226_OK;	
 }
 
 
@@ -159,10 +158,10 @@ enum INA226_status ina226_reset(struct INA226 * ina226)
 {
 	ina226->config.bits.RST = 1;
 	if(writereg(ina226->address, INA226_CONFIG_REG, 
-					   ina226->config.buffer.all) != OK){
-		return CONFIG_ERROR;
+					   ina226->config.buffer.all) != INA226_OK){
+		return INA226_CONFIG_ERROR;
 	}
-	return OK;
+	return INA226_OK;
 }
 
 enum INA226_status ina226_get_current(struct INA226 * ina226, 
@@ -170,43 +169,43 @@ enum INA226_status ina226_get_current(struct INA226 * ina226,
 {
 	uint16_t current_reg_val;
 	if(readreg(ina226->address, INA226_CURRENT_REG, 
-					  &current_reg_val) != OK){
-		return I2C_TRANSMISSION_ERROR;
+					  &current_reg_val) != INA226_OK){
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
 	*current = (float32_t) current_reg_val*ina226->current_LSB;
-	return OK;
+	return INA226_OK;
 }
 
 enum INA226_status ina226_get_vbus(struct INA226 * ina226, float32_t * vbus)
 {
 	uint16_t vbus_reg_val;
-	if(readreg(ina226->address, INA226_VBUS_REG, &vbus_reg_val) != OK){
-		return I2C_TRANSMISSION_ERROR;
+	if(readreg(ina226->address, INA226_VBUS_REG, &vbus_reg_val) != INA226_OK){
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
 	*vbus = (float32_t) vbus_reg_val*INA226_VBUS_LSB_VAL;
-	return OK;
+	return INA226_OK;
 }
 
 enum INA226_status ina226_get_vshunt(struct INA226 * ina226, float32_t * vshunt)
 {
 	uint16_t vshunt_reg_val;
 	if(readreg(ina226->address, INA226_VSHUNT_REG, 
-					  &vshunt_reg_val) != OK){
-		return I2C_TRANSMISSION_ERROR;
+					  &vshunt_reg_val) != INA226_OK){
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
 	*vshunt = (float32_t) vshunt_reg_val*INA226_VSHUNT_LSB_VAL;
-	return OK;
+	return INA226_OK;
 }
 
 enum INA226_status ina226_get_pwr(struct INA226 * ina226, float32_t * pwr)
 {
 	uint16_t pwr_reg_val;
-	if(readreg(ina226->address, INA226_PWR_REG, &pwr_reg_val) != OK) {
-		return I2C_TRANSMISSION_ERROR;
+	if(readreg(ina226->address, INA226_PWR_REG, &pwr_reg_val) != INA226_OK) {
+		return INA226_I2C_TRANSMISSION_ERROR;
 	}
 	// power_LSB = current_LSB*25
 	*pwr= (float32_t) pwr_reg_val*ina226->current_LSB*25;
-	return OK;
+	return INA226_OK;
 }
 
 enum INA226_status ina226_set_avg(struct INA226 * ina226, enum INA226_avg avg)
@@ -266,8 +265,8 @@ enum INA226_status ina226_clear_flags(struct INA226 * ina226)
 {
 	uint16_t buffer;
 	if(readreg(ina226->address, INA226_MASK_EN_REG, 
-					  &buffer) != OK) {
-		return FLAGS_NOT_CLEARED;
+					  &buffer) != INA226_OK) {
+		return INA226_FLAGS_NOT_CLEARED;
 	}
-	return OK;
+	return INA226_OK;
 }
