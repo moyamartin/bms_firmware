cbb5d9e50f3d42d1e98f71ea7048d7ea108cab00
Martin Moya <moyamartin1@gmail.com>
2021-01-21 00:03:49 -0300

    Fixes read/write method for bq76
    * Adds manual CRC generator

--- Core/Src/crc.c
+++ Core/Src/crc.c
@@ -3,9 +3,10 @@
 uint8_t calculate_crc(uint8_t * buffer, size_t buffer_length, 
                       const uint8_t * crc_lut)
 {
-    uint8_t crc = 0;
-    for(int i = 0; i < buffer_length; ++i){
-       crc = crc_lut[crc ^ buffer[i]];
-    }
+	uint8_t crc = 0, tmp = 0;
+	for(size_t i = 0; i < buffer_length; ++i){
+		tmp = (uint8_t) (crc ^ buffer[i]);
+		crc = crc_lut[tmp];
+	}
     return crc;
 }
